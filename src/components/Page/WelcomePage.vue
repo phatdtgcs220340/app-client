725f1245bc8d272b2807f17a7b35f12fb040fb1f
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-05-29 22:32:45 +0700

    welcome page

---
+++ src/components/Page/WelcomePage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+    defineProps({
+        fullName : String
+    })
+</script>
+<template>
+    <div class="px-2 py-4">
+        <h1 class="mb-4
+            text-3xl font-medium text-gray-700">💕😘 Hello {{ fullName }}</h1>
+        <h1 class="text-2xl font-medium text-gray-700">🌠🌠 Welcome to Password Manager</h1>
+    </div>
+</template>
