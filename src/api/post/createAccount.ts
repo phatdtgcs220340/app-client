4889666c3b2cd944c3ebfe7587cf9a456968d140
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-05-30 21:02:17 +0700

    add api call for post request

---
+++ src/api/post/createAccount.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { accessToken, resourceBaseURL } from "../../env";
+
+interface Account {
+    username: String,
+    password: String,
+    application : String, 
+    type: String
+}
+/**
+ * Create account function that do POST request to the web server
+ * @param
+ * @returns A Promise 
+ */
+export async function createAccount(form : Account) {
+    return await axios.post(`${resourceBaseURL}/api/v1/account`,
+        {
+            "username" : form.username,
+            "password" : form.password,
+            "applicationName" : form.application,
+            "type" : form.type
+        },
+        {
+            headers : {
+                'Authorization' : `Bearer ${accessToken}`,
+                'Content-Type' : 'application/json'
+            }
+        })
+        .then((response) => {
+            return response.data
+        })
+}
