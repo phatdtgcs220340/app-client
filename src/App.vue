215fd7f0499afc43cc1a3e7d56676d0b2b71fa81
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-05-30 21:07:19 +0700

    add components

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,15 @@
   import SideBar from './components/SideBar/SideBar.vue';
   import WelcomePage from './components/Page/WelcomePage.vue';
   import AccountPage from './components/Page/AccountPage.vue';
-  import LoadingPage from './components/Loading/LoadingPage.vue';
-  import { setUserDetails } from './utils/userdetails';
-  import { setAccount } from './utils/account';
+  import CreateAccountPage from './components/Page/CreateAccountPage.vue';
+  import LoadingPage from './components/Page/LoadingPage.vue';
+  import ErrorPage from './components/Page/ErrorPage.vue';
+  import { setUserDetails } from './api/get/userdetails';
+  import { setAccount } from './api/get/account';
 
   const loginBoxVisible = ref(false);
   const pageState = ref('dashboard');
-  const loadCompleted = ref(false);
+  const pageStatus = ref('loading');
   const userdetails = reactive({
       fullName : '',
       username : ''
@@ -24,17 +26,23 @@
     dateAudit : ''
   })
   watch(pageState, async (state) => {
-    loadCompleted.value = false;
+    pageStatus.value = 'loading';
     console.log(state)
-    if (state == 'dashboard' && userdetails.fullName == '') {
-      await setUserDetails(userdetails); 
-      console.log(userdetails)
+    try {
+      if (state == 'dashboard' && userdetails.fullName == '') {
+        await setUserDetails(userdetails); 
+        console.log(userdetails)
+      }
+      if (state.startsWith('application')) {
+        let appId = state.split(" ")[1];
+        await setAccount(appId, account)
+        
+      }
+      pageStatus.value = 'success';
     }
-    if (state.startsWith('application')) {
-      let appId = state.split(" ")[1];
-      await setAccount(appId, account)
+    catch{
+      pageStatus.value = 'error';
     }
-    loadCompleted.value = true;
   },
   { immediate: true }
   )
@@ -48,23 +56,26 @@
     @toggle-login="() => loginBoxVisible = !loginBoxVisible"
     @toggle-dashboard="() => pageState = 'dashboard'"
     @toggle-account="(id) => pageState = `application ${id}`"
+    @toggle-create-account="() => pageState='create'"
   />
   <div class="p-4 pt-8 sm:ml-64">
     <div class="p-2 border-2 border-gray-200 border-dashed rounded-lg ">
-      <WelcomePage v-if="pageState == 'dashboard' && loadCompleted" 
+      <WelcomePage v-if="pageState == 'dashboard' && pageStatus == 'success'" 
         :full-name="userdetails.fullName"
         class="bg-gray-100 rounded-lg"/>
-      <AccountPage v-if="pageState.startsWith('application') && loadCompleted"
+      <CreateAccountPage v-if="pageState == 'create'"
+        class="bg-gray-100 rounded-lg"/>
+      <AccountPage v-if="pageState.startsWith('application') && pageStatus == 'success'"
         :id="account.id"
         :username="account.username"
         :application="account.applicationName"
         :password="account.password"
-        decrypted-password="*******"
         :type="account.type"
         :date-audit="account.dateAudit"
         class="bg-gray-100 rounded-lg"
       />
-      <LoadingPage v-if="!loadCompleted" class="bg-gray-100 rounded-lg"/>
+      <LoadingPage v-if="pageStatus == 'loading'" class="bg-gray-100 rounded-lg"/>
+      <ErrorPage v-if="pageStatus == 'error'" class="bg-gray-100 rounded-lg"/>
     </div>
   </div>
 </template>
