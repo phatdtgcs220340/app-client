7442824bcf3c72f3c0b9fc1b9456dc3cb0af2f36
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-06-03 23:14:58 +0700

    remove unused stuffs

--- src/components/Login/LoginBox.vue
+++ src/components/Login/LoginBox.vue
@@ -1,36 +1,27 @@
 <script setup lang="ts">
     import { reactive, ref } from 'vue';
-    import axios from 'axios';
     import Spinner from '../Spinner/Spinner.vue';
-    import { authBaseURL, authorLink } from '../../env';
+    import {login} from "../../logic/authenticate.ts";
     const loginForm = reactive({
         username: '',
         password: ''
     })
     const loadCompleted = ref(true);
-    async function login() {
-        try {
-            const instance = axios.create({ withCredentials : true })
-            loadCompleted.value = false;
-            await instance.post(`${authBaseURL}/api/login`, {
-                username: loginForm.username,
-                password: loginForm.password,
-            }, {
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded', // Ensure the correct content type
-                },
-                withCredentials: true, // Again, to preserve the session/cookies
-            })
-            .then(() => window.location.assign(authorLink))
-        } catch (error) {
-        } finally {
-            loadCompleted.value = true;
-        }
+    const errorMessage = ref('');
+    async function authenticate() {
+      loadCompleted.value = false;
+      await login(loginForm)
+          .catch((e) => {
+            if (e.code == 'ERR_NETWORK') {
+              errorMessage.value = "Network connection error";
+            }
+          })
+          .finally(() => loadCompleted.value = true)
     }
     </script>
 <template>
-    <div class="bg-gray-500 flex justify-center items-center h-screen z-100">
-        <form @submit.prevent="login" 
+    <div class="flex justify-center items-center h-screen z-100">
+        <form @submit.prevent="authenticate"
         class="grid grid-cols-3 w-fit p-6 rounded-lg bg-white shadow-md place-content-start">
             <label class="font-medium text-gray-600 text-lg"
                 >Username</label>
@@ -47,13 +38,16 @@
                 type="password" 
                 v-model="loginForm.password">
             <div class="text-sm text-blue-500 cursor-pointer mb-2">Forgot password?</div>
-            <button class="col-span-3 bg-gray-100 py-1 rounded-md shadow-md mb-3
+            <button class="col-span-3 bg-gray-100 py-1 rounded-md shadow-md mb-2
                            text-lg font-semibold text-gray-600 text-center
                            flex justify-center items-center h-[32px]
                            hover:bg-gray-600 hover:text-gray-100">
                 <template v-if="loadCompleted">Log in</template>
                 <template v-else><Spinner class="text-center"/></template>
             </button>
+            <div class="col-span-3 mb-1 text-sm text-red-400">
+              {{ errorMessage }}
+            </div>
             <div class="col-span-3 text-sm text-gray-600">
                 First time here? <span 
                     class="cursor-pointer
