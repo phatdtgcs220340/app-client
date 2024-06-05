86ca1f48839695af5230d4acb1d36ca04f276839
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-06-06 01:13:52 +0700

    add register

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,12 +5,14 @@ import CreateAccount from '../components/Page/CreateAccount.vue'
 import Account from '../components/Page/Account.vue'
 import LoginBox from '../components/Login/LoginBox.vue'
 import Authorizing from '../components/Loading/Authorizing.vue'
+import RegisterBox from '../components/Login/RegisterBox.vue'
 const routes = [
   { path: '/', name: '', component: Dashboard},
   { path: '/create',name: 'create', component: CreateAccount},
   { path: '/application/:id', name: 'account', component: Account },
   { path: '/login', name: 'login', component: LoginBox },
-  { path: '/authorized', name: 'authorizing', component: Authorizing}
+  { path: '/register', name: 'register', component: RegisterBox },
+  { path: '/authorized', name: 'authorizing', component: Authorizing }
 ]
 
 const router = createRouter({
